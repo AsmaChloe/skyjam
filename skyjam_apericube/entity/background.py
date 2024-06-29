bb0efe552d6322ab44c7055150f44630b6f79c4d
Corentin <dup.corentin51@gmail.com>
2024-06-29 22:19:04 +0200

    build app to .exe

---
+++ skyjam_apericube/entity/background.py
@@ -0,0 +1,27 @@
+import pygame
+
+class Background(pygame.sprite.Sprite):
+    def __init__(self, scrollSpeed):
+        super().__init__()
+        self.baseTexture = pygame.image.load("img/cailloux.png").convert()
+        self.middleTexture = pygame.image.load("graphics/background/Fond_gros.png")
+        self.image = pygame.Surface((4*288 + 1092, 500*288))
+        self.rect = self.image.get_rect(midtop = (1920/2, 0))
+        self.scrollSpeed = scrollSpeed
+        
+        #le point de début de la zone est au pixel 414 et la fin au pixel 1506 et la zone fait 1092 pixels de large
+        
+        for i in range(5):
+            for j in range(500):
+                if i == 2  and j%4 == 0:
+                    self.image.blit(self.middleTexture, (288*2, 288 * j))
+                if i < 2:
+                    self.image.blit(self.baseTexture, (288 * i, 288 * j))
+                if i > 2:
+                    self.image.blit(self.baseTexture, (288 * (i % 2) + 1668, 288 * j))
+    
+    def setScrollSpeed(self, scrollSpeed):
+        self.scrollSpeed = scrollSpeed
+        
+    def update(self):
+        self.rect.top -= self.scrollSpeed
---
+++ skyjam_apericube/entity/pickaxe.py
@@ -0,0 +1,63 @@
+import pygame
+import math
+
+class Pickaxe(pygame.sprite.Sprite):
+    def __init__(self, initPos, destination):
+        super().__init__()
+        self.tickFrame = 0
+        self.mvtDir = 1
+        self.imageCollection = []
+        self.initPos = initPos
+        self.initX , self.initY = initPos
+        self.destX, self.destY = destination
+        self.playerX, self.playerY = initPos
+        self.xSpeed = 0
+        #self.turning = False
+        
+        for i in range(1, 9):
+            self.imageCollection.append(pygame.image.load(f"graphics/wood_pickaxe/Animation_bois{i}.png").convert_alpha())
+            
+        self.image = self.imageCollection[0]
+        self.rect = self.image.get_rect(midtop = initPos)
+
+    def switchDir(self):
+        self.mvtDir = -1
+    
+    def animate(self):
+        #durée de l'animation avec une vitesse de 20 pixels par frame : 65 frames (un peu plus d'une seconde)
+        self.tickFrame = (self.tickFrame + 0.5) % 8
+        self.image = self.imageCollection[int(self.tickFrame)]
+        if self.rect.bottom > 1000:                                  #inversion du mvtDir quand on a atteind la portée maximale de la pioche
+            self.switchDir()
+            
+        self.rect.bottom += 20 * self.mvtDir                         #vitese de déplacement vertical * le mvtDir
+        
+        if self.mvtDir == 1:
+            #formule vitesse x => Vy * (dx/dy) => Vy fixe, dx distance entre xDestination et xDépart, dy la hauteur de travel (fixe, limite-centreDuPersonnage)
+            xSpeedThrow = ((self.destX - self.initX)*20)/740
+            print(xSpeedThrow)
+            self.rect.centerx += xSpeedThrow
+        else:
+            if self.playerX < self.rect.centerx:
+                #effet progressif du changement de vitesse pour faire faire des petites courbes à la pioche (bien plus beau)
+                if self.xSpeed != -20:
+                    self.xSpeed -= 2
+
+            elif self.playerX > self.rect.centerx:
+                if self.xSpeed != 20:
+                    self.xSpeed += 2
+  
+                
+            self.rect.centerx += self.xSpeed
+        
+        if self.rect.centery <= self.initY:
+            self.rect.centery = self.initY
+            if self.rect.centerx < self.playerX + 20 and self.rect.centerx > self.playerX - 20:
+                #kill() permet de supprimer le sprite de tous les groupes dans lequel il est présent
+                self.kill()
+
+    def updatePlayerPos(self, positionJoueur):
+        self.playerX, self.playerY = positionJoueur
+    
+    def update(self):
+        self.animate()
