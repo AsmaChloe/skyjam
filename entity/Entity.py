52f95702103c38943e9670b2204f836e4da3f53a
MirageHD68 <hugo.delay@icloud.com>
2024-06-30 17:42:58 +0200

    Conflict

--- entity/Entity.py
+++ entity/Entity.py
@@ -94,16 +94,8 @@ class Entity(pygame.sprite.Sprite):
         self.hurt_sounds = [pygame.mixer.Sound("sound/Aie_1.wav"), pygame.mixer.Sound("sound/Aie_2.wav"), pygame.mixer.Sound("sound/Aie_3.wav")]
 
     def animate(self):
-<<<<<<< HEAD
-        if(self.isEvolvingPickaxe)  :
-            self.image = self.currentPickaxeType.pickaxeEvolvingImageCollection[int(self.evolutionTickFrame)]
-            self.evolutionTickFrame = (self.evolutionTickFrame + 0.2) % 18
-            if(int(self.evolutionTickFrame) == 17):
-                self.isEvolvingPickaxe = False
-                self.evolutionTickFrame = 0
-                self.currentPickaxeType = self.game.pickaxe_type
-        else :
-=======
+
+
 
         
         if self.isDynamiteStarting or self.isDynamiteDuring or self.isDynamiteEnding:
@@ -135,47 +127,32 @@ class Entity(pygame.sprite.Sprite):
                     self.dynamiteTickFrame = 0
                     self.isDynamite = False
                     self.isDynamiteEnding = False
-
+        elif self.isDynamite
         else :
-            
->>>>>>> parent of eedcc1e (Merge branch 'master' into dynamite)
             self.image = pygame.Surface((290, 320), pygame.SRCALPHA, 32).convert_alpha()
-            if not self.isWithBat:
-                #animation lancé
-                if self.isThrowing:
-                    self.throwTickFrame = (self.throwTickFrame + 0.2)
-                    self.image.blit(self.imageCollectionThrowing[int(self.throwTickFrame)], self.imageCollectionThrowing[int(self.throwTickFrame)].get_rect(center = (290/2, 320/2)))
+            if(self.isEvolvingPickaxe)  :
+                self.image.blit(self.currentPickaxeType.pickaxeEvolvingImageCollection[int(self.evolutionTickFrame)],self.currentPickaxeType.pickaxeEvolvingImageCollection[int(self.evolutionTickFrame)].get_rect(center = (290/2, 320/2)))
+                self.evolutionTickFrame = (self.evolutionTickFrame + 0.2) % 18
+                if(int(self.evolutionTickFrame) == 17):
+                    self.isEvolvingPickaxe = False
+                    self.evolutionTickFrame = 0
+                    self.currentPickaxeType = self.game.pickaxe_type
+            else:     
+                if not self.isWithBat:
+                    #animation lancé
+                    if self.isThrowing:
+                        self.throwTickFrame = (self.throwTickFrame + 0.2)
+                        self.image.blit(self.imageCollectionThrowing[int(self.throwTickFrame)], self.imageCollectionThrowing[int(self.throwTickFrame)].get_rect(center = (290/2, 320/2)))
 
-                    #4 frames sur l'animation donc on limite l'index à 3
-                    if int(self.throwTickFrame) == 3:
-                        self.throwTickFrame = 0
-                        self.isThrowing = False
+                        #4 frames sur l'animation donc on limite l'index à 3
+                        if int(self.throwTickFrame) == 3:
+                            self.throwTickFrame = 0
+                            self.isThrowing = False
+                    else:
+                        #animation IDLE
+                        self.tickFrame = (self.tickFrame + 0.2) % 18
+                        self.image.blit(self.imageCollection[int(self.tickFrame)], self.imageCollection[int(self.tickFrame)].get_rect(center = (290/2, 320/2)))
                 else:
-<<<<<<< HEAD
-                    #animation IDLE
-                    self.tickFrame = (self.tickFrame + 0.2) % 18
-                    self.image.blit(self.imageCollection[int(self.tickFrame)], self.imageCollection[int(self.tickFrame)].get_rect(center = (290/2, 320/2)))
-            else:
-                if self.isThrowing:
-                    self.throwTickFrame = (self.throwTickFrame + 0.5)
-                    self.batTickFame = (self.batTickFame + 0.2) % 12
-                    self.image.blit(self.imageCollectionBatThrowing[int(self.batTickFame)%2], self.imageCollectionBatThrowing[int(self.batTickFame)%2].get_rect(center = (290/2, 320/2)))
-                    if int(self.throwTickFrame) == 3:
-                        self.throwTickFrame = 0
-                        self.isThrowing = False
-                else:
-                    self.batTickFame = (self.batTickFame + 0.2) % 12
-                    self.image.blit(self.imageCollectionBat[int(self.batTickFame)], self.imageCollectionBat[int(self.batTickFame)].get_rect(center = (290/2, 320/2)))
-
-            if self.isProtected:
-                self.protectionTickFrame = (self.protectionTickFrame + 0.15) % 18
-                if self.isWithBat:
-                    shield = pygame.transform.rotozoom(self.imageCollectionProtected[int(self.protectionTickFrame)], 0, 1.5)
-                else:
-                    shield = self.imageCollectionProtected[int(self.protectionTickFrame)]
-                self.image.blit(shield, shield.get_rect(center = (290/2, 320/2)))
-            self.mask = pygame.mask.from_surface(self.image)
-=======
                     if self.isThrowing:
                         self.throwTickFrame = (self.throwTickFrame + 0.5)
                         self.batTickFame = (self.batTickFame + 0.2) % 12
@@ -193,12 +170,9 @@ class Entity(pygame.sprite.Sprite):
                         shield = pygame.transform.rotozoom(self.imageCollectionProtected[int(self.protectionTickFrame)], 0, 1.5)
                     else:
                         shield = self.imageCollectionProtected[int(self.protectionTickFrame)]
-
                     self.image.blit(shield, shield.get_rect(center = (290/2, 320/2)))
-
                 self.mask = pygame.mask.from_surface(self.image)
->>>>>>> parent of eedcc1e (Merge branch 'master' into dynamite)
-        
+            
     def touchBat(self, isBatTouched):
         self.isWithBat = isBatTouched
         
