9a105fd5352a09d215eb3f161cd8356a94454b1a
AsmaChloe <asmachloe@gmail.com>
2024-06-30 17:51:53 +0200

    updates image with fondu

--- animations/StatingAnimation.py
+++ animations/StatingAnimation.py
@@ -5,7 +5,7 @@ from utils.CustomSprite import CustomSprite
 
 class StartingAnimation(CustomSprite):
     def __init__(self, position):
-        self.image_collection = [pygame.image.load(f"graphics/character/starting_animation/animation intro{i}.png") for i in range(1, 42)]
+        self.image_collection = [pygame.image.load(f"graphics/character/starting_animation/animation intro{i}.png") for i in range(1, 46)]
         self.img_index = 0
         image = self.image_collection[self.img_index]
         name = "StartingAnimation"
@@ -16,6 +16,6 @@ class StartingAnimation(CustomSprite):
     def update(self, events):
         self.image = self.image_collection[(int)(self.img_index)]
         self.img_index += 0.2
-        if self.img_index >= 41:
+        if self.img_index >= 45:
             self.kill()
         # self.rect.center = self.rect.center
